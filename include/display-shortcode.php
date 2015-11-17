4ec18b66060ec6009cd8cdae62ce19e6ebc0b716
Fabio Mattei <burattino@gmail.com>
2015-11-17 09:44:59 +0100

    cleaning css

--- include/display-shortcode.php
+++ include/display-shortcode.php
@@ -29,9 +29,9 @@ function RCOF_offers_list( $atts, $content ) {
 	        $out .= '<li id="#slide'.$i.'">';
 
 			$out .= '<div class="offer-box">
-				<div class="offer-thumbnail">'.get_the_post_thumbnail( $post_id, 'offer-img', array( 'class' => 'offer-thumb' ) ).'</div>
-	                <div class="offer-desc">
-				    	<h5><a href="'.get_permalink().'" title="' . get_the_title() . '">'.get_the_title() .'</a></h5>
+					<div class="offer-thumbnail offer-box-child">'.get_the_post_thumbnail( $post_id, 'offer-img', array( 'class' => 'offer-thumb' ) ).'</div>
+	                <div class="offer-desc offer-box-child">
+				    	<h6><a href="'.get_permalink().'" title="' . get_the_title() . '">'.get_the_title() .'</a></h6>
 				    <p>'.get_the_content().'</p>
 				    </div> <!-- .offer-desc -->
 				</div> <!-- .offer-box -->';
